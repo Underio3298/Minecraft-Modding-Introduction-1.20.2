375caf3fa4f4f40c829f318713f42297d5510c49
Underio3298 <underio3298@gmail.com>
2024-02-04 11:13:33 -0500

    Add Ruby item

--- src/main/java/net/aaron/minecraftmod/item/ModItems.java
+++ src/main/java/net/aaron/minecraftmod/item/ModItems.java
@@ -2,14 +2,20 @@ package net.aaron.minecraftmod.item;
 
 import net.aaron.minecraftmod.MinecraftMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-
     public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries) {
+        entries.add(RUBY);
+    }
 
     private static Item registerItem (String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(MinecraftMod.MOD_ID, name), item);
@@ -17,6 +23,9 @@ public class ModItems {
 
     public static void registerModItems() {
         MinecraftMod.LOGGER.info("Registering Mod Items for " + MinecraftMod.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientTabItemGroup);
+
     }
+
 
 }
