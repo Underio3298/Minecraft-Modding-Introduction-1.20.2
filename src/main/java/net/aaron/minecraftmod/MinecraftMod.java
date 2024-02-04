82b323a781a296b9eb59b7e54a994fc6bb5b87c5
Underio3298 <underio3298@gmail.com>
2024-02-03 21:36:38 -0500

    Initial commit

---
+++ src/main/java/net/aaron/minecraftmod/MinecraftMod.java
@@ -0,0 +1,16 @@
+package net.aaron.minecraftmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MinecraftMod implements ModInitializer {
+	public static final String MOD_ID = "minecraft-mod";
+   public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/aaron/minecraftmod/MinecraftModClient.java
@@ -0,0 +1,10 @@
+package net.aaron.minecraftmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
