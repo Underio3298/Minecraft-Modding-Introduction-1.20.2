923c44014551152ad6d0fd800d13d3b5df09e1eb
Underio3298 <underio3298@gmail.com>
2024-02-06 07:22:57 -0500

    Add Ruby Block and Raw Ruby Block

--- src/main/java/net/aaron/minecraftmod/block/ModBlocks.java
+++ src/main/java/net/aaron/minecraftmod/block/ModBlocks.java
@@ -2,14 +2,23 @@ package net.aaron.minecraftmod.block;
 
 import net.aaron.minecraftmod.MinecraftMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
+
+    public static final Block RUBY_BLOCK = registerBlock("ruby_block",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+    public static final Block RAW_RUBY_BLOCK = registerBlock("raw_ruby_block",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
     private static Block registerBlock(String name, Block block){
         registerBlockItem(name, block);
--- src/main/java/net/aaron/minecraftmod/item/ModItemGroups.java
+++ src/main/java/net/aaron/minecraftmod/item/ModItemGroups.java
@@ -1,6 +1,7 @@
 package net.aaron.minecraftmod.item;
 
 import net.aaron.minecraftmod.MinecraftMod;
+import net.aaron.minecraftmod.block.ModBlocks;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -16,12 +17,14 @@ public class ModItemGroups {
     /**
      * The RUBY_GROUP is a tab in the creative inventory that contains the RUBY and RAW_RUBY items.
      */
-    public static final ItemGroup RUBY_GROUP = Registry.register(Registries.ITEM_GROUP, // name of the item group
-            new Identifier(MinecraftMod.MOD_ID, "ruby"), // id of the item group
-            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.ruby")) // display name of the item group
-                    .icon(() -> new ItemStack(ModItems.RUBY)).entries((displayContext, entries) -> { // function that adds the items to the creative tab
+    public static final ItemGroup MINECRAFT_MOD_GROUP = Registry.register(Registries.ITEM_GROUP, // name of the item group
+            new Identifier(MinecraftMod.MOD_ID, "minecraftmod"), // id of the item group
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.minecraftmod")) // display name of the item group
+                    .icon(() -> new ItemStack(ModItems.RAW_RUBY)).entries((displayContext, entries) -> { // function that adds the items to the creative tab
                         entries.add(ModItems.RUBY);
                         entries.add(ModItems.RAW_RUBY);
+                        entries.add(ModBlocks.RUBY_BLOCK);
+                        entries.add(ModBlocks.RAW_RUBY_BLOCK);
                     }).build());
 
     /**
