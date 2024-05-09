2da8b7cacf641b264df7d270145e4edd15b84871
Aaron Avdeev <underio3298@gmail.com>
2024-05-09 13:02:48 -0400

    Add block breaking to ruby and war ruby blocks

--- src/main/java/net/aaron/minecraftmod/MinecraftMod.java
+++ src/main/java/net/aaron/minecraftmod/MinecraftMod.java
@@ -13,25 +13,25 @@ import org.slf4j.LoggerFactory;
  */
 public class MinecraftMod implements ModInitializer {
 
-    /**
-     * The unique ID of the mod.
-     */
-    public static final String MOD_ID = "minecraft-mod";
+  /**
+   * The unique ID of the mod.
+   */
+  public static final String MOD_ID = "minecraft-mod";
 
-    /**
-     * The logger for the mod.
-     */
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+  /**
+   * The logger for the mod.
+   */
+  public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    @Override
-    public void onInitialize() {
-        // Register the items
-        ModItems.registerModItems();
-        // Register the item groups
-        ModItemGroups.registerItemGroups();
-        // Register the blocks
-        ModBlocks.registerModBlocks();
-        // Log that the mod has been initialized
-        LOGGER.info(MOD_ID + " has been initialized.");
-    }
+  @Override
+  public void onInitialize() {
+    // Register the items
+    ModItems.registerModItems();
+    // Register the item groups
+    ModItemGroups.registerItemGroups();
+    // Register the blocks
+    ModBlocks.registerModBlocks();
+    // Log that the mod has been initialized
+    LOGGER.info(MOD_ID + " has been initialized.");
+  }
 }
